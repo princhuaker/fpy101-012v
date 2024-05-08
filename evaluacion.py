73dde10f2ab4d2ce99fa860a4c936cd1b5b5c452
princhuaker <167265937+princhuaker@users.noreply.github.com>
2024-05-07 22:13:04 -0400

    Add files via upload

---
+++ evaluacion.py
@@ -0,0 +1,56 @@
+while mostrar_menu ():
+    print("menu:")
+print("1.Pikachu Roll $4500")
+print("2.otaku rolls $5000")
+print("3.pulpo venenoso roll $5200")
+print("4.anguila eletrica $4800") 
+print(" finaliza perdido")
+
+mostrar_menu ()
+opcion =  input(" selecione una opcion de (1-4):")
+
+if opcion == "1":
+    print(" has selcionado la opcion 1.")
+elif opcion == "2":
+    print(" has selcionado la opcion 2.")
+elif opcion== "3":
+    print (" has selecionado la opcion 4")
+elif opcion == " 4":
+    print("has selecionado opcion 4")
+else : 
+ print("su pedido no se encuentra disponible")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pikachu rolss
+# otaku rolls
+# pulpo venenoso roll
+# anguila eletrica
+
